d1f82a1ac3fcba97a81fe2ec07839212965efc65
cde-la-r <cde-la-r@student.42.fr>
2024-08-30 12:27:44 +0200

    add old structs just in case

--- include/philo.h
+++ include/philo.h
@@ -25,6 +25,30 @@ typedef struct s_philo
 	pthread_mutex_t	*right_fork;
 }	t_philo;
 
+/*
+typedef struct s_args
+{
+        int     n;
+        int     t_die;
+        int     t_eat;
+        int     t_sleep;
+        int     max_eat;
+}       t_args;
+
+typedef struct s_philo
+{
+        int     n;
+        int     state;
+        int     eats;
+}       t_philo;
+
+typedef struct s_table
+{
+        t_philo *philo;
+        int             *forks;
+}       t_table;
+*/
+
 int		init_resources(pthread_t **threads, pthread_mutex_t **forks, t_philo **philos, int num_philos);
 void	create_philosophers(pthread_t *threads, t_philo *philos, pthread_mutex_t *forks, int num_philos);
 void	join_philosophers(pthread_t *threads, int num_philos);
