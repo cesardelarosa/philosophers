01e21971ecd0d6358542f3a6b480df0d496dcac9
Cesar De La Rosa Sobrino <cde-la-r@c2r3s3.42madrid.com>
2025-04-17 22:34:45 +0200

    init norminette

--- philo_bonus/src/core/init_simulation.c
+++ philo_bonus/src/core/init_simulation.c
@@ -6,7 +6,7 @@
 /*   By: cde-la-r <code@cesardelarosa.xyz>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 22:02:50 by cde-la-r          #+#    #+#             */
-/*   Updated: 2025/03/27 10:52:16 by cesi             ###   ########.fr       */
+/*   Updated: 2025/04/17 20:42:33 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-bool	init_simulation(t_table *table)
+static void	unlink_sems(void)
 {
-	unsigned int	i;
-
 	sem_unlink("/forks_sem");
 	sem_unlink("/print_sem");
 	sem_unlink("/meal_sem");
 	sem_unlink("/full_sem");
 	sem_unlink("/room_sem");
+}
+
+static bool	open_sems(t_table *table)
+{
 	table->forks_sem = sem_open("/forks_sem", O_CREAT, 0644, table->n_philos);
 	table->print_sem = sem_open("/print_sem", O_CREAT, 0644, 1);
 	table->meal_sem = sem_open("/meal_sem", O_CREAT, 0644, 1);
@@ -39,10 +41,22 @@ bool	init_simulation(t_table *table)
 		|| table->meal_sem == SEM_FAILED || table->room_sem == SEM_FAILED
 		|| (table->n_meals != -1 && table->full_sem == SEM_FAILED))
 		return (false);
+	return (true);
+}
+
+static bool	alloc_structs(t_table *table)
+{
 	table->pids = malloc(sizeof(pid_t) * table->n_philos);
 	table->philos = malloc(sizeof(t_philo) * table->n_philos);
 	if (!table->pids || !table->philos)
 		return (false);
+	return (true);
+}
+
+static void	init_philos(t_table *table)
+{
+	unsigned int	i;
+
 	i = 0;
 	while (i < table->n_philos)
 	{
@@ -53,5 +67,17 @@ bool	init_simulation(t_table *table)
 		table->philos[i].full = 0;
 		i++;
 	}
+}
+
+bool	init_simulation(t_table *table)
+{
+	if (table == NULL)
+		return (false);
+	unlink_sems();
+	if (!open_sems(table))
+		return (false);
+	if (!alloc_structs(table))
+		return (false);
+	init_philos(table);
 	return (true);
 }
