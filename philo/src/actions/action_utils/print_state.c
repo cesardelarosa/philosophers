534a633f75f13715e312b9b01d2200dba98e8f29
Cesar De La Rosa Sobrino <cde-la-r@c2r3s3.42madrid.com>
2025-04-18 00:59:36 +0200

    no fucking shit printing after death

--- philo/src/actions/action_utils/print_state.c
+++ philo/src/actions/action_utils/print_state.c
@@ -6,7 +6,7 @@
 /*   By: cde-la-r <code@cesardelarosa.xyz>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 22:54:15 by cde-la-r          #+#    #+#             */
-/*   Updated: 2025/03/26 10:46:31 by cesi             ###   ########.fr       */
+/*   Updated: 2025/04/18 00:59:08 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@ bool	print_state(t_philo *philo, const char *state)
 {
 	long	timestamp;
 
-	if (check_stop(philo->table))
-		return (false);
 	lock_safe_mutex(&philo->table->print_mtx);
+	if (check_stop(philo->table))
+	{
+		unlock_safe_mutex(&philo->table->print_mtx);
+		return (false);
+	}
 	timestamp = (get_time() - philo->table->start_time) / 1000;
 	printf("%ld %d %s\n", timestamp, philo->id, state);
 	unlock_safe_mutex(&philo->table->print_mtx);
